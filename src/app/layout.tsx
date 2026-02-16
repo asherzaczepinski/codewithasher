ae88fd20b2de93ecc2623323022bfe4ce8af7913
asherzaczepinski <azaczepinski@gmail.com>
2026-02-16 12:57:49 -0800

    great

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
         <Script
           src="https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.js"
           strategy="beforeInteractive"
